5020c54b19e52208a24a1879572d3942cf0fc1ec
Phil Isaac <isaac.phil@gmail.com>
2013-11-30 17:24:30 -0500

    Add calendar base

---
+++ src/com/timetracks/CalendarFragment.java
@@ -0,0 +1,20 @@
+package com.timetracks;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CalendarFragment extends Fragment {
+
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+		// I think I want to return a LinearLayout
+        View rootView = inflater.inflate(R.layout.activity_calendar_fragment, container, false);
+		
+        return rootView;
+    }
+}
