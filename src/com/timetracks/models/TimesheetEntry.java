c71dc377ac892d78c2a3e9c0f0f3d114c279ad6d
Ivan <ivanpeng13@gmail.com>
2013-11-30 18:50:16 -0500

    Add Serializable implementation. Necessary for passing intents via UI.

--- src/com/timetracks/models/TimesheetEntry.java
+++ src/com/timetracks/models/TimesheetEntry.java
@@ -1,10 +1,16 @@
 package com.timetracks.models;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import com.roscopeco.ormdroid.Entity;
 
-public class TimesheetEntry extends Entity {
+public class TimesheetEntry extends Entity implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4371373555974701792L;
+	
 	public Date startDate;
 	public Date endDate;
 	public Project project;
