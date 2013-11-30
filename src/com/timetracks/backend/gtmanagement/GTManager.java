8771d22c210a2ad5e1427b2e67808bd2073aca8f
Wesley George <wesley.george@gmail.com>
2013-11-30 13:35:33 -0500

    Stub for backend team for GT management.

---
+++ src/com/timetracks/backend/gtmanagement/GTManager.java
@@ -0,0 +1,12 @@
+package com.timetracks.backend.gtmanagement;
+
+import java.util.List;
+import com.timetracks.models.TimeSpaceProjectAssociation;
+
+public class GTManager {
+	public List<TimeSpaceProjectAssociation> getAssociations(DateTime startTime, DateTime endTime) {
+		// return summaries if this period has been summarized
+		// calculate (i.e. cluster and tag) data that has not
+		return null; 
+	}
+}
