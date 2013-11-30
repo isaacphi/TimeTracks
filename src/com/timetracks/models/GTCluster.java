81152bd3266ce476cfc6d3d1fcb303a6f883b7f2
Wesley George <wesley.george@gmail.com>
2013-11-30 14:54:39 -0500

    Reverting GTCluster object to remove schema data.

--- src/com/timetracks/models/GTCluster.java
+++ src/com/timetracks/models/GTCluster.java
@@ -2,8 +2,6 @@ package com.timetracks.models;
 
 import java.util.Date;
 
-import android.provider.BaseColumns;
-
 public class GTCluster {
 	public int id;
 	public double c_x;
@@ -12,14 +10,4 @@ public class GTCluster {
 	public Date startDate;
 	public Date endDate;
 	public boolean userProvided;
-	
-	public static abstract class GTClusterSchema implements BaseColumns {
-		public static final String TABLE_NAME="GTCluster";
-		public static final String COLUMN_NAME_C_X="cx";
-		public static final String COLUMN_NAME_C_Y="cy";
-		public static final String COLUMN_NAME_RADIUS="radius";
-		public static final String COLUMN_NAME_START_TIME="start_time";
-		public static final String COLUMN_NAME_END_TIME="end_time";
-		public static final String COLUMN_NAME_USER_SET="set_by_user";
-	}
 }
