ed968f771446dce8fb5277556b30d7288008ff1b
ASelwa <selwa.alex@gmail.com>
2013-11-30 17:10:50 -0500

    Hardcoded data

--- src/com/timetracks/bootstrapping/DatabaseBootstrap.java
+++ src/com/timetracks/bootstrapping/DatabaseBootstrap.java
@@ -1,9 +1,13 @@
 package com.timetracks.bootstrapping;
 
+import java.util.List;
+
+import com.timetracks.models.GTCluster;
+
 public class DatabaseBootstrap {
 	private static final String GTCLUSTER_FILENAME="";
 	public static void BootstrapDatabase() {
-		List<GTCluster> = GTClusterXmlAdapter.loadFromFile(GTCLUSTER_FILENAME);
+		List<GTCluster> = GTClusterXMLLoader.loadFromFile(GTCLUSTER_FILENAME);
 		// and more 
 		
 		
