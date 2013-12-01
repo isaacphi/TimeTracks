823e1eb90d84655b064df30910114f2a6c24d5b6
ASelwa <selwa.alex@gmail.com>
2013-11-30 23:35:10 -0500

    Clusterer made.

--- src/com/timetracks/backend/gtmanagement/Clusterer.java
+++ src/com/timetracks/backend/gtmanagement/Clusterer.java
@@ -1,5 +1,7 @@
 package com.timetracks.backend.gtmanagement;
 
+import java.util.ArrayList;
+import java.lang.Math;
 import java.util.List;
 
 import com.timetracks.models.GTCluster;
@@ -7,7 +9,109 @@ import com.timetracks.models.GTPoint;
 
 
 public class Clusterer {
+	
+	private static double average(List<Double> list) {
+		
+		double sum = 0;
+		for (int i = 0; i < list.size(); i++) {
+			sum += list.get(i);
+		}
+		return sum/list.size();
+	}
+	
+	private static double maximum(List<Double> list) {
+		double max = list.get(0);
+		for (int i = 1; i < list.size(); i++) {
+			if (list.get(i) > max)
+				max = list.get(i);
+		}
+		return max;
+	}
+	
+	private static double minimum(List<Double> list) {
+		double min = list.get(0);
+		for (int i = 1; i < list.size(); i++) {
+			if (list.get(i) < min)
+				min = list.get(i);
+		}
+		return min;
+	}
+	
+	private static double max(double one, double two) {
+		if (one > two)
+			return one;
+		else
+			return two;
+	}
+	
+	private static double absolute(double number) {
+		if (number > 0) 
+			return number;
+		else
+			return number*-1;	
+	}
+	
 	static List<GTCluster> cluster(List<GTPoint> pointList) {
-		return null;
+		List<GTCluster> gtcClusterList = new ArrayList<GTCluster>();
+		
+		// Variables 
+		double distThreshold = 0.0005;
+		int timeStepThreshold = 15;
+		boolean newLocation = false;
+		int count = 0;
+		List<Double> xList = null;
+		List<Double> yList = null;
+		
+		for (int i = 0; i < pointList.size(); i++) {
+			// Check if x and y are within a certain change in distance (distParam)
+			if (absolute(pointList.get(i).x - pointList.get(i+1).x) < distThreshold) {
+				count++;
+				xList.add(pointList.get(i).x);
+				yList.add(pointList.get(i).y);
+				
+				if (count > timeStepThreshold) {
+					newLocation = true;
+				}
+			}
+			else {
+				if (newLocation == true) {
+					// Add the location
+					GTCluster cluster = new GTCluster();
+					cluster.c_x = average(xList); // needs averaging
+					cluster.c_y = average(yList);
+					// Radius should be something smart, like stddev/sqrt(N), (or based on measurement error)
+					cluster.radius = max(maximum(xList)-minimum(xList), maximum(yList)-minimum(yList));
+					cluster.userProvided = false;
+					cluster.startDate = pointList.get(i-count).dates;
+					cluster.endDate = pointList.get(i).dates;
+					gtcClusterList.add(cluster);
+					newLocation = false;
+					count = 0;
+					xList = null;
+					yList = null;
+				}
+			}
+			// Catch the EOF
+			if (i == pointList.size()-1) {
+				if (newLocation == true) {
+					// Add the location
+					GTCluster cluster = new GTCluster();
+					cluster.c_x = average(xList); // needs averaging
+					cluster.c_y = average(yList);
+					// Radius should be something smart, like stddev/sqrt(N), (or based on measurement error)
+					cluster.radius = max(maximum(xList)-minimum(xList), maximum(yList)-minimum(yList));
+					cluster.userProvided = false;
+					cluster.startDate = pointList.get(i-count).dates;
+					cluster.endDate = pointList.get(i).dates;
+					gtcClusterList.add(cluster);
+					newLocation = false;
+					count = 0;
+					xList = null;
+					yList = null;
+				}	
+			}	
+		}
+		
+		return gtcClusterList;
 	}
 }
