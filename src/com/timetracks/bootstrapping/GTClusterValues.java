a9d30b12c8720362e465e7b36461e61dc1f791bf
Wesley George <wesley.george@gmail.com>
2013-12-01 12:23:21 -0500

    Change month constants by 1, so our data is for a week starting from a Sunday for better exposition purposes now that the backend returns week-aligned TimesheetEntries.

--- src/com/timetracks/bootstrapping/GTClusterValues.java
+++ src/com/timetracks/bootstrapping/GTClusterValues.java
@@ -22,8 +22,8 @@ public class GTClusterValues {
 		// Monday
 		cluster = new GTCluster();
 		cluster.id = 1;
-		cluster.startDate = getDate("2013-11-25-1-0-0");
-		cluster.endDate = getDate("2013-11-25-9-0-0");
+		cluster.startDate = getDate("2013-10-25-1-0-0");
+		cluster.endDate = getDate("2013-10-25-9-0-0");
 		cluster.c_x = 40.75311; // Alley
 		cluster.c_y = -73.98921;
 		cluster.radius = 0.0001;
@@ -33,8 +33,8 @@ public class GTClusterValues {
 		
 		cluster = new GTCluster();
 		cluster.id = 2;
-		cluster.startDate = getDate("2013-11-25-9-0-0");
-		cluster.endDate = getDate("2013-11-25-17-0-0");
+		cluster.startDate = getDate("2013-10-25-9-0-0");
+		cluster.endDate = getDate("2013-10-25-17-0-0");
 		cluster.c_x = 40.74833; // Empire
 		cluster.c_y = -73.98556;
 		cluster.radius = 0.0001;
@@ -43,8 +43,8 @@ public class GTClusterValues {
 		
 		cluster = new GTCluster();
 		cluster.id = 3;
-		cluster.startDate = getDate("2013-11-25-17-0-0");
-		cluster.endDate = getDate("2013-11-25-23-0-0");
+		cluster.startDate = getDate("2013-10-25-17-0-0");
+		cluster.endDate = getDate("2013-10-25-23-0-0");
 		cluster.c_x = 40.75311; // Alley
 		cluster.c_y = -73.98921;
 		cluster.radius = 0.0001;
@@ -54,8 +54,8 @@ public class GTClusterValues {
 		// Tuesday
 		cluster = new GTCluster();
 		cluster.id = 4;
-		cluster.startDate = getDate("2013-11-26-1-0-0");
-		cluster.endDate = getDate("2013-11-26-9-0-0");
+		cluster.startDate = getDate("2013-10-26-1-0-0");
+		cluster.endDate = getDate("2013-10-26-9-0-0");
 		cluster.c_x = 40.75311; // Alley
 		cluster.c_y = -73.98921;
 		cluster.radius = 0.0001;
@@ -64,8 +64,8 @@ public class GTClusterValues {
 		
 		cluster = new GTCluster();
 		cluster.id = 5;
-		cluster.startDate = getDate("2013-11-26-9-0-0");
-		cluster.endDate = getDate("2013-11-26-12-30-0");
+		cluster.startDate = getDate("2013-10-26-9-0-0");
+		cluster.endDate = getDate("2013-10-26-12-30-0");
 		cluster.c_x = 40.74833; // Empire
 		cluster.c_y = -73.98556;
 		cluster.radius = 0.0001;
@@ -74,8 +74,8 @@ public class GTClusterValues {
 		
 		cluster = new GTCluster();
 		cluster.id = 6;
-		cluster.startDate = getDate("2013-11-26-12-30-0");
-		cluster.endDate = getDate("2013-11-26-13-30-0");
+		cluster.startDate = getDate("2013-10-26-12-30-0");
+		cluster.endDate = getDate("2013-10-26-13-30-0");
 		cluster.c_x = 40.78271; // Central Park
 		cluster.c_y = -73.96531;
 		cluster.radius = 0.0001;
@@ -84,8 +84,8 @@ public class GTClusterValues {
 		
 		cluster = new GTCluster();
 		cluster.id = 7;
-		cluster.startDate = getDate("2013-11-26-13-30-0");
-		cluster.endDate = getDate("2013-11-26-18-0-0");
+		cluster.startDate = getDate("2013-10-26-13-30-0");
+		cluster.endDate = getDate("2013-10-26-18-0-0");
 		cluster.c_x = 40.74833; // Empire
 		cluster.c_y = -73.98556;
 		cluster.radius = 0.0001;
@@ -94,8 +94,8 @@ public class GTClusterValues {
 		
 		cluster = new GTCluster();
 		cluster.id = 8;
-		cluster.startDate = getDate("2013-11-26-18-0-0");
-		cluster.endDate = getDate("2013-11-26-23-30-0");
+		cluster.startDate = getDate("2013-10-26-18-0-0");
+		cluster.endDate = getDate("2013-10-26-23-30-0");
 		cluster.c_x = 40.75311; // Alley
 		cluster.c_y = -73.98921;
 		cluster.radius = 0.0001;
@@ -105,8 +105,8 @@ public class GTClusterValues {
 		//Wednesday
 		cluster = new GTCluster();
 		cluster.id = 9;
-		cluster.startDate = getDate("2013-11-27-1-0-0");
-		cluster.endDate = getDate("2013-11-27-9-0-0");
+		cluster.startDate = getDate("2013-10-27-1-0-0");
+		cluster.endDate = getDate("2013-10-27-9-0-0");
 		cluster.c_x = 40.75311; // Alley
 		cluster.c_y = -73.98921;
 		cluster.radius = 0.0001;
@@ -115,8 +115,8 @@ public class GTClusterValues {
 		
 		cluster = new GTCluster();
 		cluster.id = 10;
-		cluster.startDate = getDate("2013-11-27-9-0-0");
-		cluster.endDate = getDate("2013-11-27-17-0-0");
+		cluster.startDate = getDate("2013-10-27-9-0-0");
+		cluster.endDate = getDate("2013-10-27-17-0-0");
 		cluster.c_x = 40.74833; // Empire
 		cluster.c_y = -73.98556;
 		cluster.radius = 0.0001;
@@ -125,8 +125,8 @@ public class GTClusterValues {
 		
 		cluster = new GTCluster();
 		cluster.id = 11;
-		cluster.startDate = getDate("2013-11-27-17-0-0");
-		cluster.endDate = getDate("2013-11-27-23-0-0");
+		cluster.startDate = getDate("2013-10-27-17-0-0");
+		cluster.endDate = getDate("2013-10-27-23-0-0");
 		cluster.c_x = 40.75311; // Alley
 		cluster.c_y = -73.98921;
 		cluster.radius = 0.0001;
@@ -137,8 +137,8 @@ public class GTClusterValues {
 		//Thursday
 		cluster = new GTCluster();
 		cluster.id = 12;
-		cluster.startDate = getDate("2013-11-28-1-0-0");
-		cluster.endDate = getDate("2013-11-28-9-3-0");
+		cluster.startDate = getDate("2013-10-28-1-0-0");
+		cluster.endDate = getDate("2013-10-28-9-3-0");
 		cluster.c_x = 40.75311; // Alley
 		cluster.c_y = -73.98921;
 		cluster.radius = 0.0001;
@@ -147,8 +147,8 @@ public class GTClusterValues {
 		
 		cluster = new GTCluster();
 		cluster.id = 13;
-		cluster.startDate = getDate("2013-11-28-9-3-0");
-		cluster.endDate = getDate("2013-11-28-18-0-0");
+		cluster.startDate = getDate("2013-10-28-9-3-0");
+		cluster.endDate = getDate("2013-10-28-18-0-0");
 		cluster.c_x = 40.74833; // Empire
 		cluster.c_y = -73.98556;
 		cluster.radius = 0.0001;
@@ -157,8 +157,8 @@ public class GTClusterValues {
 		
 		cluster = new GTCluster();
 		cluster.id = 14;
-		cluster.startDate = getDate("2013-11-28-17-0-0");
-		cluster.endDate = getDate("2013-11-28-23-0-0");
+		cluster.startDate = getDate("2013-10-28-17-0-0");
+		cluster.endDate = getDate("2013-10-28-23-0-0");
 		cluster.c_x = 40.75311; // Alley
 		cluster.c_y = -73.98921;
 		cluster.radius = 0.0001;
@@ -168,8 +168,8 @@ public class GTClusterValues {
 		//Friday
 		cluster = new GTCluster();
 		cluster.id = 15;
-		cluster.startDate = getDate("2013-11-29-1-0-0");
-		cluster.endDate = getDate("2013-11-29-9-0-0");
+		cluster.startDate = getDate("2013-10-29-1-0-0");
+		cluster.endDate = getDate("2013-10-29-9-0-0");
 		cluster.c_x = 40.75311; // Alley
 		cluster.c_y = -73.98921;
 		cluster.radius = 0.0001;
@@ -178,8 +178,8 @@ public class GTClusterValues {
 		
 		cluster = new GTCluster();
 		cluster.id = 16;
-		cluster.startDate = getDate("2013-11-29-9-0-0");
-		cluster.endDate = getDate("2013-11-29-12-0-0");
+		cluster.startDate = getDate("2013-10-29-9-0-0");
+		cluster.endDate = getDate("2013-10-29-12-0-0");
 		cluster.c_x = 40.75201; // Random Dentist in Manhattan
 		cluster.c_y = -73.99118;
 		cluster.radius = 0.0001;
@@ -188,8 +188,8 @@ public class GTClusterValues {
 		
 		cluster = new GTCluster();
 		cluster.id = 17;
-		cluster.startDate = getDate("2013-11-29-12-0-0");
-		cluster.endDate = getDate("2013-11-29-18-0-0");
+		cluster.startDate = getDate("2013-10-29-12-0-0");
+		cluster.endDate = getDate("2013-10-29-18-0-0");
 		cluster.c_x = 40.74833; // Empire
 		cluster.c_y = -73.98556;
 		cluster.radius = 0.0001;
@@ -198,8 +198,8 @@ public class GTClusterValues {
 		
 		cluster = new GTCluster();
 		cluster.id = 18;
-		cluster.startDate = getDate("2013-11-29-18-0-0");
-		cluster.endDate = getDate("2013-11-29-23-0-0");
+		cluster.startDate = getDate("2013-10-29-18-0-0");
+		cluster.endDate = getDate("2013-10-29-23-0-0");
 		cluster.c_x = 40.75311; // Alley
 		cluster.c_y = -73.98921;
 		cluster.radius = 0.0001;
