f1ac435753b3547662f848c112a0b75f19fbe62e
Ivan <ivanpeng13@gmail.com>
2013-12-01 01:08:18 -0500

    Added animation capabilities for PopUpWindow. Will duplicate in calendar fragment and map fragment.

--- src/com/timetracks/GoogleMapFragment.java
+++ src/com/timetracks/GoogleMapFragment.java
@@ -2,12 +2,17 @@ package com.timetracks;
 
 import java.util.List;
 
+import net.londatiga.android.ActionItem;
+import net.londatiga.android.QuickAction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -21,6 +26,11 @@ public class GoogleMapFragment extends Fragment {
 
 	private GoogleMap googleMap;
 	static final LatLng TORONTO = new LatLng(43.670852, -79.376435);
+	private static final int ID_VIEW_OTHER_FRAGMENT = 1;
+	private static final int ID_TAG = 2;
+	private static final int ID_EXCLUDE = 3;
+	
+	
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,6 +43,15 @@ public class GoogleMapFragment extends Fragment {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+        
+        Button button = (Button) rootView.findViewById(R.id.tempbutton);
+        button.setOnClickListener(new OnClickListener() {
+		QuickAction quickAction = setQuickActionButtons();	
+			@Override
+			public void onClick(View v) {
+				quickAction.show(v);
+			}
+		});
         return rootView;
     }
 
@@ -80,14 +99,58 @@ public class GoogleMapFragment extends Fragment {
 				
 				MarkerOptions marker = new MarkerOptions().position(new LatLng(gtCluster.c_x, gtCluster.c_y));
 				marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+				Toast.makeText(getActivity().getApplicationContext(), marker.toString(), Toast.LENGTH_LONG).show();
+				googleMap.addMarker(marker);
 			}
-			
-			
-			
-			
-			
+		
 		}
 		
 	}
+	public QuickAction setQuickActionButtons()	{
+		
+		Intent intent = getActivity().getIntent();
+		
+		// Should have variable text and logos...in onResume?
+		ActionItem viewOtherFragment;
+		if (intent.getStringExtra("Activity") == "GoogleMapFragment")	
+			viewOtherFragment = new ActionItem(ID_VIEW_OTHER_FRAGMENT, "Map", getResources().getDrawable(R.drawable.ic_action_map));
+		else
+			viewOtherFragment = new ActionItem(ID_VIEW_OTHER_FRAGMENT, "Calendar", getResources().getDrawable(R.drawable.ic_action_event));
+		
+		ActionItem tagItem;
+		// Need to detect things from DB before wiring this up. We'll use the same symbol though
+		tagItem = new ActionItem(ID_TAG, "Tag", getResources().getDrawable(R.drawable.ic_action_new_label));
+		ActionItem exclude = new ActionItem(ID_EXCLUDE, "Exclude", getResources().getDrawable(R.drawable.ic_action_cancel));
+		
+		final QuickAction quickAction = new QuickAction(getActivity(), QuickAction.HORIZONTAL);
+		
+		quickAction.addActionItem(viewOtherFragment);
+		quickAction.addActionItem(tagItem);
+		quickAction.addActionItem(exclude);
+		
+		quickAction.setOnActionItemClickListener(new QuickAction.OnActionItemClickListener() {
+			@Override
+			public void onItemClick(QuickAction quickAction, int pos, int actionId) {
+				ActionItem actionItem = quickAction.getActionItem(pos);
+				
+				if (actionId == ID_VIEW_OTHER_FRAGMENT) {
+					// Start Activity of view Other Fragment
+					
+				} else if (actionId == ID_TAG) {
+					// Select Activity of Tagging or Retagging
+				} else {
+					// Start Activity of Exclude
+				}
+			}
+		});
+		
+		quickAction.setOnDismissListener(new QuickAction.OnDismissListener() {			
+			@Override
+			public void onDismiss() {
+				// Nothing to display, just close the window naturally
+			}
+		});
+		return quickAction;
+	} 
 }
 
