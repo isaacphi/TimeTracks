0ae35655c19e5234f9f9e6d597968a15afdfc464
Wesley George <wesley.george@gmail.com>
2013-11-30 20:36:00 -0500

    Contract for clusterer and tagger components.

---
+++ src/com/timetracks/backend/gtmanagement/Tagger.java
@@ -0,0 +1,12 @@
+package com.timetracks.backend.gtmanagement;
+
+import java.util.List;
+
+import com.timetracks.models.GTCluster;
+import com.timetracks.models.GTTimesheetLink;
+
+public class Tagger {
+	static List<GTTimesheetLink> createTimesheetEntriesFor(List<GTCluster> clusterList) {
+		return null;
+	}
+}
