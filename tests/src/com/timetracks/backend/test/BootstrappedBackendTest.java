b0c64fde710cb05786ee0a73208f94eaca1105e9
Wesley George <wesley.george@gmail.com>
2013-12-01 11:58:07 -0500

    Implement getMaxTimesheetEntryDate correctly.

--- tests/src/com/timetracks/backend/test/BootstrappedBackendTest.java
+++ tests/src/com/timetracks/backend/test/BootstrappedBackendTest.java
@@ -1,9 +1,13 @@
 package com.timetracks.backend.test;
 
+import java.util.Date;
+import java.util.List;
+
 import android.test.ApplicationTestCase;
 
 import com.roscopeco.ormdroid.Entity;
 import com.roscopeco.ormdroid.ORMDroidApplication;
+import com.timetracks.Helpers;
 import com.timetracks.backend.BootstrappableBackend;
 import com.timetracks.bootstrapping.GTClusterValues;
 import com.timetracks.bootstrapping.GTTimesheetLinkValues;
@@ -15,6 +19,10 @@ import com.timetracks.test.ORMDroidApplicationTestCaseHelpers;
 
 public class BootstrappedBackendTest extends ApplicationTestCase<ORMDroidApplication>{
 	BootstrappableBackend backend;
+	List<GTCluster> e_cluster_list   = GTClusterValues.getClusters();
+	List<GTTimesheetLink> e_link_list= GTTimesheetLinkValues.getGTTimesheetLinkValues();
+	List<TimesheetEntry> e_timesheet_list=TimesheetEntryValues.getEntries(); 
+
 	
 	public BootstrappedBackendTest() {
 		super(ORMDroidApplication.class);
@@ -29,15 +37,33 @@ public class BootstrappedBackendTest extends ApplicationTestCase<ORMDroidApplica
 		ORMDroidApplicationTestCaseHelpers.clearAllTables();		
 	}
 	
-	public void testBootstrap() {
+	public void testBootstrapBasic() {
+		assertEquals(0, Entity.query(GTTimesheetLink.class).executeMulti().size());
 		assertEquals(0, Entity.query(GTCluster.class).executeMulti().size());
 		assertEquals(0, Entity.query(TimesheetEntry.class).executeMulti().size());
-		assertEquals(0, Entity.query(GTTimesheetLink.class).executeMulti().size());
 		
 		backend.bootstrap();
 		
-		assertEquals(GTClusterValues.getClusters().size(), Entity.query(GTCluster.class).executeMulti().size());
-		assertEquals(TimesheetEntryValues.getEntries().size(), Entity.query(TimesheetEntry.class).executeMulti().size());
-		assertEquals(GTTimesheetLinkValues.getGTTimesheetLinkValues().size(), Entity.query(GTTimesheetLink.class).executeMulti().size());		
+		assertEquals(e_cluster_list.size(), Entity.query(GTCluster.class).executeMulti().size());
+		assertEquals(e_timesheet_list.size(), Entity.query(TimesheetEntry.class).executeMulti().size());
+		assertEquals(e_link_list.size(), Entity.query(GTTimesheetLink.class).executeMulti().size());		
+	}
+	
+	public void testGetTimesheetEntries() {
+		backend.bootstrap();
+		assertEquals(e_timesheet_list.size(),
+					 backend.getAllTimesheetEntries().size());
+	}
+	
+	public void testGetTimesheetEntriesByDate() {
+		backend.bootstrap();
+		assertEquals(e_cluster_list.size(),
+				backend.getTimesheetEntries(new Date(0), new Date(Long.MAX_VALUE)).size());
+	}
+	
+	public void getMaxDate() {
+		backend.bootstrap();
+		Date endDate = backend.getMaxTimesheetEntryDate();
+		assertEquals(Helpers.dateHelper("2013-11-29-23-0-0"), endDate);
 	}
 }
