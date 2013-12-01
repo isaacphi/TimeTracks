b16d87b422883fbb95911940fdcfe90c3e3166ca
ASelwa <selwa.alex@gmail.com>
2013-11-30 20:09:31 -0500

    minor edits to values (data)

--- src/com/timetracks/bootstrapping/TimeSheetEntryValues.java
+++ src/com/timetracks/bootstrapping/TimeSheetEntryValues.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+import com.timetracks.models.Project;
 import com.timetracks.models.TimesheetEntry;
 
 public class TimesheetEntryValues {
@@ -19,20 +20,26 @@ public class TimesheetEntryValues {
 		List<TimesheetEntry> list = new ArrayList<TimesheetEntry>();
 		TimesheetEntry entry = new TimesheetEntry();
 		
+		Project proj = new Project();
+		entry.project = proj;
+		
+		
 		// Monday
+		entry.id = 1;
 		entry.startDate = getDate("2013-11-25-1-0-0");
 		entry.endDate = getDate("2013-11-25-9-0-0");
 		entry.note = "This is a note.";
-		//entry.project = new Project()
 		entry.colourCode = "green";
 		list.add(entry);
 		
+		entry.id = 2;
 		entry.startDate = getDate("2013-11-25-9-0-0");
 		entry.endDate = getDate("2013-11-25-17-0-0");
 		entry.note = "This is a note.";
 		entry.colourCode = "blue";
 		list.add(entry);
 		
+		entry.id = 3;
 		entry.startDate = getDate("2013-11-25-17-0-0");
 		entry.endDate = getDate("2013-11-25-23-0-0");
 		entry.note = "This is a note.";
@@ -40,30 +47,35 @@ public class TimesheetEntryValues {
 		list.add(entry);
 		
 		// Tuesday
+		entry.id = 4;
 		entry.startDate = getDate("2013-11-26-1-0-0");
 		entry.endDate = getDate("2013-11-26-9-0-0");
 		entry.note = "This is a note.";
 		entry.colourCode = "green";
 		list.add(entry);
 		
+		entry.id = 5;
 		entry.startDate = getDate("2013-11-26-9-0-0");
 		entry.endDate = getDate("2013-11-26-12-30-0");
 		entry.note = "This is a note.";
 		entry.colourCode = "blue";
 		list.add(entry);
 		
+		entry.id = 6;
 		entry.startDate = getDate("2013-11-26-12-30-0");
 		entry.endDate = getDate("2013-11-26-13-30-0");
 		entry.note = "This is a note.";
 		entry.colourCode = "orange";
 		list.add(entry);
 		
+		entry.id = 7;
 		entry.startDate = getDate("2013-11-26-13-30-0");
 		entry.endDate = getDate("2013-11-26-18-0-0");
 		entry.note = "This is a note.";
 		entry.colourCode = "blue";
 		list.add(entry);
 		
+		entry.id = 8;
 		entry.startDate = getDate("2013-11-26-18-0-0");
 		entry.endDate = getDate("2013-11-26-23-30-0");
 		entry.note = "This is a note.";
@@ -71,6 +83,7 @@ public class TimesheetEntryValues {
 		list.add(entry);
 		
 		//Wednesday
+		entry.id = 9;
 		entry.startDate = getDate("2013-11-27-1-0-0");
 		entry.endDate = getDate("2013-11-27-9-0-0");
 		entry.note = "This is a note.";
@@ -78,12 +91,14 @@ public class TimesheetEntryValues {
 		entry.colourCode = "green";
 		list.add(entry);
 		
+		entry.id = 10;
 		entry.startDate = getDate("2013-11-27-9-0-0");
 		entry.endDate = getDate("2013-11-27-17-0-0");
 		entry.note = "This is a note.";
 		entry.colourCode = "blue";
 		list.add(entry);
 		
+		entry.id = 11;
 		entry.startDate = getDate("2013-11-27-17-0-0");
 		entry.endDate = getDate("2013-11-27-23-0-0");
 		entry.note = "This is a note.";
@@ -92,6 +107,7 @@ public class TimesheetEntryValues {
 		
 		
 		//Thursday
+		entry.id = 12;
 		entry.startDate = getDate("2013-11-28-1-0-0");
 		entry.endDate = getDate("2013-11-28-9-3-0");
 		entry.note = "This is a note.";
@@ -99,12 +115,14 @@ public class TimesheetEntryValues {
 		entry.colourCode = "green";
 		list.add(entry);
 		
+		entry.id = 13;
 		entry.startDate = getDate("2013-11-28-9-3-0");
 		entry.endDate = getDate("2013-11-28-18-0-0");
 		entry.note = "This is a note.";
 		entry.colourCode = "blue";
 		list.add(entry);
 		
+		entry.id = 14;
 		entry.startDate = getDate("2013-11-28-17-0-0");
 		entry.endDate = getDate("2013-11-28-23-0-0");
 		entry.note = "This is a note.";
@@ -112,6 +130,7 @@ public class TimesheetEntryValues {
 		list.add(entry);
 		
 		//Friday
+		entry.id = 15;
 		entry.startDate = getDate("2013-11-29-1-0-0");
 		entry.endDate = getDate("2013-11-29-9-0-0");
 		entry.note = "This is a note.";
@@ -119,18 +138,21 @@ public class TimesheetEntryValues {
 		entry.colourCode = "green";
 		list.add(entry);
 		
+		entry.id = 16;
 		entry.startDate = getDate("2013-11-29-9-0-0");
 		entry.endDate = getDate("2013-11-29-12-0-0");
 		entry.note = "This is a note.";
 		entry.colourCode = "red";
 		list.add(entry);
 		
+		entry.id = 17;
 		entry.startDate = getDate("2013-11-29-12-0-0");
 		entry.endDate = getDate("2013-11-29-18-0-0");
 		entry.note = "This is a note.";
 		entry.colourCode = "blue";
 		list.add(entry);
 		
+		entry.id = 18;
 		entry.startDate = getDate("2013-11-29-18-0-0");
 		entry.endDate = getDate("2013-11-29-23-0-0");
 		entry.note = "This is a note.";
