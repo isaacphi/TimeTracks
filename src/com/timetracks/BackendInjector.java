df1c14c7b593450849e959a5c0ffa0fcee02fbcc
Wesley George <wesley.george@gmail.com>
2013-12-01 09:44:28 -0500

    Guard bootstrapping of database with a preference flag to avoid repeating the bootstrapping from run to run.

--- src/com/timetracks/BackendInjector.java
+++ src/com/timetracks/BackendInjector.java
@@ -5,6 +5,7 @@ import com.timetracks.backend.BootstrappedBackend;
 
 public class BackendInjector {
 	public static BackendInterface getBackend() {
-		return BootstrappedBackend.getInstance();
+		//return BootstrappedBackend.getInstance();
+		return new BootstrappedBackend();
 	}
 }
--- src/com/timetracks/backend/BootstrappedBackend.java
+++ src/com/timetracks/backend/BootstrappedBackend.java
@@ -1,41 +1,36 @@
 package com.timetracks.backend;
 
-import java.util.Date;
 import java.util.List;
 
-import com.timetracks.bootstrapping.GTClusterValues;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.roscopeco.ormdroid.ORMDroidApplication;
 import com.timetracks.bootstrapping.GTTimesheetLinkValues;
-import com.timetracks.bootstrapping.TimesheetEntryValues;
-import com.timetracks.models.GTCluster;
 import com.timetracks.models.GTTimesheetLink;
-import com.timetracks.models.Project;
-import com.timetracks.models.TimesheetEntry;
 
 public class BootstrappedBackend extends Backend {
-	private static BackendInterface singleton=null;
-	public static BackendInterface getInstance() {
+	//private static BackendInterface singleton=null;
+	private static final String PREFERENCE_FILE="BootstrappedBackendPreferences";
+	/*public static BackendInterface getInstance() {
 		if (singleton==null) {
 			singleton=new BootstrappedBackend();
 		}
 		return singleton;
-	}
-	private BootstrappedBackend() {
-		if(!databaseBootstrapped()) {
-			bootstrapDatabase();
+	}*/
+	public BootstrappedBackend() {
+		SharedPreferences preferences = ORMDroidApplication.getSingleton().getApplicationContext().getSharedPreferences(PREFERENCE_FILE, 0);
+		if(!preferences.getBoolean("isBootstrapped", false)) {
+			Log.d("BootstrappedDatabase", "Bootstrapping database");
+			List<GTTimesheetLink> linkList = GTTimesheetLinkValues.getGTTimesheetLinkValues();
+			for(GTTimesheetLink link : linkList) {
+				link.timesheetEntry.save();
+				link.gtCluster.save();
+				link.save();
+			}
+			SharedPreferences.Editor edit = preferences.edit();
+			edit.putBoolean("isBootstrapped", true);
+			edit.commit();
 		}
 	}
-	
-	boolean databaseBootstrapped() {
-		return false;
-	}
-	
-	void bootstrapDatabase() {
-		// bootstrap database;
-		List<GTTimesheetLink> linkList = GTTimesheetLinkValues.getGTTimesheetLinkValues();
-		for(GTTimesheetLink link : linkList) {
-			link.timesheetEntry.save();
-			link.gtCluster.save();
-			link.save();
-		}
-	}	
 }
